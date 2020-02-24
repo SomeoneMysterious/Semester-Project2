e7c3fc5f45a852f4f4e1b56ab7078ee1c408abbc
William Krietemeyer <22krietemeyer_willia@dcs.dublinschools.net>
2020-02-24 11:38:49 -0500

    Screen size works

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import time
 tk = Tk()
 tk.title("Protect The Base!")
 x = tk.winfo_screenwidth()
-y = tk.winfo_screenheight()
+y = tk.winfo_screenheight()-63
 ratio = x/y
 print(ratio)
 print(str(x)+"-"+str(y))
@@ -13,8 +13,8 @@ midox = x/2
 midoy = y/2
 canvas = Canvas(tk, width=x, height=y)
 tk.resizable(width=False, height=False)
-#tk.geometry(str(x)+'x'+str(y))
 canvas.pack()
+tk.geometry("+-7+0")
 line = canvas.create_line(0, 0, x, y)
 tk.update()
 lives = 50
@@ -26,7 +26,7 @@ for lives in range(lives, -1, -1):
     x = ogX/ogLives*lives
     midx = x/2
     midy = y/2
-    offx = int(midox-midx)
+    offx = int(midox-midx)-7
     offy = int(midoy-midy)
     canvas.config(width=x, height=y)
     tk.geometry('+'+str(offx)+'+'+str(offy))
@@ -39,4 +39,3 @@ print(str(x)+'-'+str(y))
 canvas.config(width=ogX, height=ogY)
 tk.geometry("+0+0")
 tk.update()
-time.sleep(10)
