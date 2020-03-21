b700f37fbee8ea195e352b67766651c7df9a22da
SomeoneElse <ilovebadgerswill@gmail.com>
2020-03-20 23:38:04 -0400

    Made very basic title screen

--- Main.py
+++ Main.py
@@ -23,21 +23,33 @@ class Game:
 
     def __init__(self):
         tk.title("Protect The Base!")
-        self.canvas = Canvas(tk, width=500, height=100)
+        self.canvas = Canvas(tk, width=x, height=y)
         tk.resizable(width=False, height=False)
         self.canvas.pack()
         tk.geometry("+-7+0")
-        self.enemyCtrl = Enemys()
+        self.clickChecked = True
+        self.clickX = 0
+        self.clickY = 0
+        tk.bind("<Button 1>", self.mouseClick)
+        self.enemyCtrl = Enemies()
         self.tower = Tower(self)
         self.titleScreen()
 
     def titleScreen(self):
-        tk.update()
+        self.canvas.create_text(midx, midy-200, text='Protect the base!', font=('Helvetica', 35), tag="titleText")
+        self.canvas.create_text(midx, midy-140, text='Click anywhere to continue.', font=('Helvetica', 20), tag="titleText")
+        while self.clickChecked:
+            tk.update()
+        self.clickChecked = True
         self.gameRunning = True
+        self.tower.startGame()
+        self.enemyCtrl.startGame()
+        self.canvas.delete("titleText")
         self.handleGame()
 
     def handleGame(self):
         while self.gameRunning:
+            self.enemyCtrl.moveEnemies()
             if self.health != self.lastHealth:
                 self.lastHealth = self.health
                 self.shrinkWindow()
@@ -67,8 +79,13 @@ class Game:
         self.enemyCtrl.winShrinkMove()
 
     def shrinkTest(self):
-        if random.randint(1, 50) == 10:
+        if random.randint(1, 100) == 10:
             self.health -= 1
+
+    def mouseClick(self, clickloc):
+        self.clickChecked = False
+        self.clickX = clickloc.x
+        self.clickY = clickloc.y
 
     def endScreen(self):
         tk.destroy()
@@ -76,19 +93,32 @@ class Game:
 
 
 class Tower:
-    def __init__(self, game):
-        self.game = game
+    def __init__(self, gamein):
+        self.game = gamein
         self.tower = self.game.canvas.create_rectangle(midx-25, midy-25, midx+25, midy+25, fill="green")
+        self.hideTower()
         tk.update()
 
     def moveTower(self):
-        print(midx)
         self.game.canvas.coords(self.tower, (midx-25, midy-25, midx+25, midy+25))
         tk.update()
 
+    def startGame(self):
+        self.showTower()
 
-class Enemys:
+    def hideTower(self):
+        self.game.canvas.itemconfigure(self.tower, state='hidden')
+
+    def showTower(self):
+        self.game.canvas.itemconfigure(self.tower, state='normal')
+
+
+class Enemies:
+
     def __init__(self):
+        pass
+
+    def startGame(self):
         pass
 
     def moveEnemies(self):
